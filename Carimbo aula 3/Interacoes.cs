4afc727ba468aea29ece738c4863bff48d2a5b6b
Phyllipe <filipeetx@gmail.com>
2020-05-16 16:25:41 -0300

    Atividade 3

---
+++ Carimbo aula 3/Interacoes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carimbo_aula_3
+{
+    interface Interacoes
+    {
+        void usar();
+        void carregar(string Pcor, int Pcarga);
+        void trocarCor(string Pcor);
+        void trocarTexto(string Ptexto);
+    }
+}
---
+++ Carimbo aula 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Carimbo_aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informar o texto do carimbo: ");
+            string texto1 = Console.ReadLine();
+            Console.WriteLine("Informar a cor: ");
+            string cor1 = Console.ReadLine();
+            Console.WriteLine("Informar a quantidade de cargas: ");
+            int carga1 = Convert.ToInt32(Console.ReadLine());
+            Carimbo num1 = new Carimbo(texto1);
+
+
+            num1.carregar(cor1, carga1);
+            num1.usar();
+        }
+    }
+}
